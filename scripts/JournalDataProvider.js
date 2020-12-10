741945f8cbd842544d2d24856417591424d9e580
Xine75 <cjdoza@gmail.com>
2020-12-10 16:00:40 -0600

    starting api

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -6,42 +6,32 @@
  *      the entries for different purposes.
  */
 
-// This is the original data.
-const journal = [
-    {
-        id: 1,
-        date: "11/16/2020",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Inspired"
-    },
 
-    {
-        id: 2,
-        date: "11/17/2020",
-        concept: "HTML & JavaScript",
-        entry: "We talked about how to automate information on a page using JavaScript",
-        mood: "Frustrated"
-    },
+let journalArray = []
 
-    {
-        id: 3,
-        date: "11/18/2020",
-        concept: "More JavaScript",
-        entry: "We worked in labs to create a Daily Journal, applying what we have learned from Martin's Aquarium",
-        mood: "Satisfied"
-    }
+export const useJournalEntries = () => journalArray.slice()
 
-]
-
-/*
-    You export a function that provides a version of the
-    raw data in the format that you want
-*/
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+export const getEntries = () => {
+    return fetch("http://localhost:8088/entries") // Fetch from the API
+        .then(response => response.json())  // Parse as JSON
+        .then(entries => {
+            console.table(entries)
+            journalArray = entries
+            
+        })
 }
+
+
+
+
+
+/* (--This is what we did before we had an external API--)
+    You export a function that provides a version of the raw data in the format that you want
+*/
+// export const useJournalEntries = () => {
+//     const sortedByDate = journalArray.sort(
+//         (currentEntry, nextEntry) =>
+//             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+//     )
+//     return sortedByDate
+// }
